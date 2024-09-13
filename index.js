ad40a692e5a60a28668cfbe43b4bae022cf7198c
kzone159 <kzone159@gmail.com>
2024-09-13 13:35:59 -0400

    Here is is the landing page for Odin project

---
+++ index.js
@@ -0,0 +1,51 @@
+// random number gen
+
+
+const person = {
+     firstname:"Kevin",
+
+    hi: function(){
+        console.log("Here");
+     }
+
+
+
+
+
+}
+
+
+
+
+
+// const sub = document.getElementById("checkbox");
+
+// const mastercard = document.getElementById("mastercard");
+
+// const visa = document.getElementById("visa");
+
+// const paypal = document.getElementById("paypal");
+
+// const mySubmit = document.getElementById("buttonsubmit");
+
+
+// const paymentresult = document.getElementById("result");
+
+
+// mySubmit.onclick = function(){
+//     if(sub.checked){
+//         paymentresult.textContent = "clicked the subscribe button";
+//     }
+//     else if(mastercard.checked){
+//         paymentresult.textContent = "You clicked on mastercard";
+//     }
+
+//     else if(visa.checked){
+//         paymentresult.textContent = "You clicked on visa";
+//     }
+
+
+
+// }
+
+
